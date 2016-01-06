7dd162d0566af90c301fca9ee6782859666fde8e
unknown <amhe3@697-334-Bilbo.sf.k12.internal>
2016-01-06 10:47:03 -0800

    save

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,19 +1,21 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
-    noLoop();
-    String digits = e.substring(2, 12);
-    double dNum = Double.parseDouble(digits);
-    
+    noLoop();    
 }  
 public void draw()  
 {   
-	//not needed for this assignment
-	for(int i = 3; i < (e.length-1)/2; i++)
-	{
-        digits = e.substring(i, 10+i);
-	}
-	System.out.println(dNum);
+	// for(int i = 3; i < (e.size-1)/2; i++)
+	// {
+ //        digits = e.substring(i, 10+i);
+ //        if(isPrime == true)
+ //        {
+ //            System.out.println(dNum);
+ //        }
+	// }
+    String digits = e.substring(2, 12);
+    double dNum = Double.parseDouble(digits);
+    System.out.println(dNum);
 }  
 public boolean isPrime(double dNum)  
 {   
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -12,3 +12,47 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
+public class GoogleBillboard extends PApplet {
+
+public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public void setup()  
+{            
+    noLoop();
+    
+    
+    
+}  
+public void draw()  
+{   
+	// for(int i = 3; i < (e.size-1)/2; i++)
+	// {
+ //        digits = e.substring(i, 10+i);
+ //        if(isPrime == true)
+ //        {
+ //            System.out.println(dNum);
+ //        }
+	// }
+    String digits = e.substring(2, 12);
+    double dNum = Double.parseDouble(digits);
+    System.out.println(dNum);
+}  
+public boolean isPrime(double dNum)  
+{   
+    for(int i = 2; i < Math.sqrt(dNum); i++)
+    {
+    	if(dNum % i == 0)
+    	{
+    		return false;
+    	}
+    }  
+    return true;  
+} 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
